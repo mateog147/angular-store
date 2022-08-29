1114b02e45cb106895143173b7a93ae069b5865b
mateosofka <mateo.gutierrez@sofka.com.co>
2022-08-29 14:08:08 -0500

    sweet alert library added to project, and detail comonent complememnted with preliminary function for add to cart

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -2,6 +2,7 @@ import { Product } from './../../models/product';
 import { ProductService } from 'src/app/services/product.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import swal from'sweetalert2';
 
 @Component({
   selector: 'app-detail',
@@ -25,6 +26,17 @@ export class DetailComponent implements OnInit {
   getProductInfo(){
     this.productsServie.getProductById(this.productId)
     .subscribe((data) => this.product = data)
+  }
+
+  addToCart(){
+    swal.fire(
+      '',
+      'Product added to cart',
+      'success'
+    ).then(()=> {
+      console.log(this.product);
+      this.router.navigate(['/products']);
+     })
   }
 
 }
